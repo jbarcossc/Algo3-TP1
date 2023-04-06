6c4955a8d6c6e391a43d7bda0ce6d24591d9d735
Lautaro Diaz Bonelli <lautaro.diazbonelli@gmail.com>
2023-04-06 01:05:14 -0300

    backtracking

--- ej2/dinamica.cpp
+++ ej2/dinamica.cpp
@@ -1,3 +1,17 @@
 #include "dinamica.h"
 
-bool dinamica(vector<int> &v, int m, int r);
+bool dinamica(vector<int> &v, int &m, int &r){
+    return PD(v,0,0,&m,&r);
+}
+
+bool PD(vector<int> &v, int i, int k, int &m, int &r){
+    if (i==0) return PD(v,i+1,v[i],m,r);
+    if (i==v.size()) return (k % m == r);
+
+    bool res = false;
+    res = res or PD(v,i+1,k + v[i],m,r);
+    res = res or PD(v,i+1,k - v[i],m,r);
+    res = res or PD(v,i+1,k * v[i],m,r);
+    res = res or PD(v,i+1,k^(v[i]),m,r);
+    return res;
+}
--- ej2/dinamica.h
+++ ej2/dinamica.h
@@ -6,6 +6,7 @@
 #ifndef ALGO3_TP1_AUXILIARES_H
 #define ALGO3_TP1_AUXILIARES_H
 
-bool dinamica(vector<int> &v, int m, int r);
+bool dinamica(vector<int> &v, int &m, int &r);
+bool PD(vector<int> &v, int i, int k, int &m, int &r);
 
 #endif //ALGO3_TP1_AUXILIARES_H
