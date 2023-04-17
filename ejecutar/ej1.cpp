feceb85cb2acf541e7dc9c6417e2ac41112701ec
jbarcossc <juancruzbarcos@hotmail.com>
2023-04-16 22:05:43 -0300

    Fix 1 optimizacion

--- ejecutar/ej1.cpp
+++ ejecutar/ej1.cpp
@@ -15,14 +15,24 @@ class NumeroMagico {
 private:
     int n;
     Cuadrado mat;
-    int sumaAecuada;
+    int sumaAdecuada;
+    bool esMagico;
+    vector<vector<int>> sumas;
+    pair<int, int> sumasDiagonales;
+
 public:
     NumeroMagico(){
         this->n = 0;
         Cuadrado matrix;
         this->mat = matrix;
-        this->sumaAecuada = 0;
+        this->sumaAdecuada = 0;
+        this->esMagico = true;
+        vector<vector<int>> sumas;
+        this->sumas = sumas;
+        pair<int, int> diags;
+        this->sumasDiagonales = diags;
     }
+
     explicit NumeroMagico(int n) {
         this->n = n;
         Cuadrado matrix;
@@ -31,100 +41,75 @@ public:
             matrix.push_back(row);
         }
         this->mat = matrix;
-        this->sumaAecuada = (pow(n, 3) + n) / 2; // JUSTIFICADO POR ENUNCIADO
+        this->sumaAdecuada = (pow(n, 3) + n) / 2; // JUSTIFICADO POR ENUNCIADO
+        this->esMagico = true;
+        vector<int> filas(n, 0);
+        vector<vector<int>> sumas(2, filas);
+        this->sumas = sumas;
+        this->sumasDiagonales = {0,0};
     }
+
     int size(){
         return this->n;
     }
-    int sumaAdecuada(){
-        return this->sumaAecuada;
-    }
+
     Cuadrado cuadrado(){
         return this->mat;
     }
+
     int size() const{
         return this->n;
     }
+
     Cuadrado cuadrado() const{
         return this->mat;
     }
 
-    bool filaLlenas(int fila){
-        for (int j = 0; j < this->n; j++) {
-            if(this->mat[fila][j] <= 0){
-                return false;
-            }
-        }
-        return true;
-    }     // Retorna si la fila pasada por paramtro esta llena
-
-
-    bool columnaLlenas(int col){
-        for (int i = 0; i < this->n; i++) {
-            if(this->mat[i][col] <= 0){
-                return false;
-            }
-        }
-        return false;
-    } // Retorna si la columna pasada por paramtro esta llena
-
-    bool estaLleno() {
-        bool res= true;
-        for (int i =0; i< this->n; i++){
-            res &= this->filaLlenas(i) ;
-        }
-        return res;
-    }
     int sumaFila(int fila){
-        int suma = 0;
-        for(int i = 0; i < this->n; i++){
-            suma += this->mat[fila][i];
-        }
-        return suma;
+        return this->sumas[0][fila];
     }
+
     int sumaCol(int columna){
-        int suma = 0;
-        for(int i = 0; i < this->n; i++){
-            suma += this->mat[i][columna];
-        }
-        return suma;
+        return this->sumas[1][columna];
     }
+
     pair<int,int> sumaDiagonal(){
-        pair<int,int> res;
-        // suma1 corresponde a la diagonal que surge desde la casilla (0,0) y recorre hasta la casilla (n-1,n-1)
-        int suma1 = 0;
-        // suma2 corresponde a la diagonal que surge desde la casilla (n-1,0) y recorre hasta la casilla (0,n-1)
-        int suma2 = 0;
-        for(int i = 0; i < this->n; i++){
-            suma1 += this->mat[i][i];
-            suma2 += this->mat[this->n - i - 1][i];
-        }
-        res.first = suma1;
-        res.second = suma2;
-        return res;
+        return this->sumasDiagonales;
     }
-    bool esMagico() {
-        if(!this->estaLleno()){
-            return false;
-        }
-        int suma = this->sumaFila(0);
-        for(int i = 0; i < this->n; i++) {
-            if(this->sumaFila(i) != suma || this->sumaCol(i) != suma){
-                return false;
-            }
-        }
-        pair<int,int> sumaDiag = this->sumaDiagonal();
-        return(sumaDiag.first == suma && sumaDiag.second == suma);
+
+    bool esCuadradoMagico() {
+        return this->esMagico;
     }
 
     void rellenarCasilla(int fila, int columna, int valor) {
-        // Debe cumplir: fila y columna en rango y valor válido
         bool filaEnRango = fila >= 0 && fila < this->n;
         bool colEnRango = columna >= 0 && columna < this->n;
         bool valorEnRango = valor > 0 && valor <= (pow(this->n,2));
         if (filaEnRango && colEnRango && valorEnRango) {
-//        cout << "[" << fila << ", " << columna << "]" << endl;
+            // ACTUALIZAR VALORES
             this->mat[fila][columna] = valor;
+            this->sumas[0][fila] += valor;
+            this->sumas[1][columna] += valor;
+            // SUMAS DIAGONALES
+            if(fila == columna) {
+                this->sumasDiagonales.first += valor;
+            }
+            if((this->n - columna - 1) == fila) {
+                this->sumasDiagonales.second += valor;
+            }
+            // ACTUALIZAR ES MAGICO
+            if(columna == (this->n - 1)) {
+                this->esMagico = this->esMagico && (this->sumas[0][fila] == this->sumaAdecuada);
+            }
+            if(fila == (this->n - 1)) {
+                this->esMagico = this->esMagico && (this->sumas[1][columna] == this->sumaAdecuada);
+            }
+            if(fila == (this->n - 1) && columna == (this->n - 1)) {
+                this->esMagico = this->esMagico && (this->sumasDiagonales.first == this->sumaAdecuada);
+            }
+            if(fila == (this->n - 1) && columna == 0) {
+                this->esMagico = this->esMagico && (this->sumasDiagonales.second == this->sumaAdecuada);
+            }
         } else {
             // PRINT ERROR: INVALID ARGUMENTS
             if (!filaEnRango) cout<< "filaEnRango" << endl;
@@ -133,6 +118,7 @@ public:
             cout << "Error al rellenar casilla [" << to_string(fila) << ", " << to_string(columna) << "] con el valor " << to_string(valor) << endl;
         }
     }
+
     bool operator< ( NumeroMagico &b){
         //asumo que son del mismo size
         for (int i = 0; i < mat.size() ; ++i) {
@@ -143,12 +129,12 @@ public:
         }
         return false;
     }
+
     NumeroMagico& operator = (const NumeroMagico& b){
         this->n = b.size();
         this->mat = b.cuadrado();
         return *this;
     }
-
 
     friend ostream &operator<<(ostream &out, const NumeroMagico& b){
         for (int i=0;i<b.n;i++){
@@ -161,53 +147,28 @@ public:
     }
 };
 
-//auxiliares
-
-void generarCuadradosRecursivos(NumeroMagico &c, vector<bool> &valores, int i, vector<NumeroMagico> &res, int &filaSumParcial, int &colSumParcial) {
+void generarCuadradosRecursivos(NumeroMagico &c, vector<bool> &valores, int i, vector<NumeroMagico> &res) {
     if (i == (pow(c.size(), 2))) {
-        if(c.esMagico()){
+        if(c.esCuadradoMagico()){
             res.push_back(c);
         }
     }
     int fila = floor(i / c.size());
-    int columna =i % c.size();
+    int columna = i % c.size();
+    if (fila >= c.size()) return;
 
-    if (fila >= c.size() ) return;
-
-//     PODAS
-//    int valorSumaAdecuado = (pow(c.size(), 3) + c.size()) / 2; // JUSTIFICADO POR ENUNCIADO
-//    // CHEQUEAR FILA RECIEN COMPLETADA
-//    bool filaRecienCompletadaMal = c.filaLlenas(fila) && columna == 0 && c.sumaFila(fila) != valorSumaAdecuado;
-//    // CHEQUEAR SI LA FILA ACTUAL YA SE PASO DEL VALOR
-//    bool filaYaSePaso = c.sumaFila(fila) > valorSumaAdecuado;
-//    // CHEQUEAR COLUMNA RECIEN COMPLETADA
-//    bool columnaRecienCompletadaMal =
-//            fila == c.size() - 1 && columna > 0 && c.sumaCol(columna - 1) != valorSumaAdecuado;
-//    // CHEQUEAR SI LA COLUMNA ACTUAL YA SE PASO DEL VALOR
-//    bool columnaYaSePaso = c.sumaCol(columna) > valorSumaAdecuado;
-//    // CHEQUEAR DIAGONAL COMPLETA
-//    bool diagonalMal = fila == (c.size() - 1) && columna > 0 && c.sumaDiagonal().second != valorSumaAdecuado;
-
-
-
-    if (!(filaRecienCompletadaMal || filaYaSePaso || columnaRecienCompletadaMal || columnaYaSePaso || diagonalMal)) {
+    if (c.esCuadradoMagico()) {
         for (int j = 0; j < valores.size(); j++) {
             if (valores[j]) {
-
-                    //poda para ver la suma parcial
-                    if (podaSumaParcial(filaSumParcial,colSumParcial, j, c.sumaAdecuada(), fila, columna, c.size())) {
-                        NumeroMagico nuevoCuadrado = c;
-                        nuevoCuadrado.rellenarCasilla(fila, columna, j + 1);
-                        vector<bool> nuevoVectorValores = valores;
-                        nuevoVectorValores[j] = false;
-                        generarCuadradosRecursivos(nuevoCuadrado, nuevoVectorValores, i + 1, res);
-                    }
+                    NumeroMagico nuevoCuadrado = c;
+                    nuevoCuadrado.rellenarCasilla(fila, columna, j + 1);
+                    vector<bool> nuevoVectorValores = valores;
+                    nuevoVectorValores[j] = false;
+                    generarCuadradosRecursivos(nuevoCuadrado, nuevoVectorValores, i + 1, res);
                 }
-
             }
         }
     }
-}
 
 bool podaSumaParcial(int &filaSum, int &colSum,  int j, int &sumaAdecuada, int &fila, int &col, int &n) {
     if (fila == 0) colSum =0;
@@ -224,15 +185,15 @@ vector<NumeroMagico> generarCuadrados(int n){
     vector<bool> vals (pow(n,2), true);
     vector<NumeroMagico> res;
 
-    auto start = high_resolution_clock::now();
+    //auto start = high_resolution_clock::now();
 
 
     generarCuadradosRecursivos(cuadrado, vals, 0, res);
 
-    auto stop2 = high_resolution_clock::now();
-    float tiempo2 = duration_cast<milliseconds>(stop2 - start).count(); //milisegundos
-    tiempo2 = tiempo2 / 1000; // pasar a segundos
-    cout << "generarCuadrado Recursivoo: " << tiempo2 << " seg\n";
+    //auto stop2 = high_resolution_clock::now();
+    //float tiempo2 = duration_cast<milliseconds>(stop2 - start).count(); //milisegundos
+    //tiempo2 = tiempo2 / 1000; // pasar a segundos
+    //cout << "generarCuadrado Recursivoo: " << tiempo2 << " seg\n";
 
     return res;
 }
@@ -240,7 +201,6 @@ vector<NumeroMagico> generarCuadrados(int n){
 bool comparador( NumeroMagico &a, NumeroMagico &b){
     return a<b;
 }
-
 
 int main() {
     //leer
@@ -265,7 +225,5 @@ int main() {
         NumeroMagico kEsimo = cuadradosMagicos[k-1];
         cout << kEsimo << endl;
     }
-
-
     return 0;
 }
