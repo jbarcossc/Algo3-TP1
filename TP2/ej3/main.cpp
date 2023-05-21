a5e29413d564354878a73eae6c65985fe23c922a
Lautaro Diaz Bonelli <lautaro.diazbonelli@gmail.com>
2023-05-21 17:08:00 -0300

    nuestro ej3 anda

--- TP2/ej3/main.cpp
+++ TP2/ej3/main.cpp
@@ -4,43 +4,108 @@
 #include <chrono>
 #include <sstream>
 #include <algorithm>
-
+#include <bits/stdc++.h>
 
 using namespace std;
-using namespace std::chrono;
 
-using Nodo = int;
-using Arista = pair<Nodo, Nodo>;
+int n, m, distanciaMax, modems, precioUTP, precioFibra;
+vector<tuple<double,int,int>> E;
+vector<pair<int,int>> posiciones;
 
+
+struct DSU{
+    DSU(int n){
+        padre = rank = vector<int>(n);
+        for(int v = 0; v < n; v++) padre[v] = v;
+    }
+
+    int find(int v){
+        if(v == padre[v]) return v;
+        return padre[v] = find(padre[v]);
+    }
+
+    void unite(int u, int v){
+        u = find(u), v = find(v);
+        if(u == v) return;
+        if(rank[u] < rank[v]) swap(u,v);
+        padre[v] = padre[u];
+        rank[u] = max(rank[u],rank[v]+1);
+    }
+
+    vector<int> padre;
+    vector<int> rank;
+};
+
+vector<double> kruskal(){
+    vector<double> res;
+    sort(E.begin(),E.end());
+    int nodosDesconectados = n;
+    DSU dsu(n);
+    for(auto i : E){
+        int u = get<1>(i), v = get<2>(i);
+        double c= get<0>(i);
+        //si (u,v) es arista segura
+        if(dsu.find(u) != dsu.find(v)){
+            // agregar
+            dsu.unite(u,v);
+            res.push_back(c);
+            nodosDesconectados--;
+            if (nodosDesconectados == modems ) break;
+        }
+    }
+    return res;
+}
+
+double distancia(pair<int,int> a, pair<int,int> b){
+    return ((double)sqrt((a.first-b.first)*(a.first-b.first) + (a.second-b.second)*(a.second-b.second)));
+}
+
+pair<double,double> precios(vector<double> dist){
+    double utp=0, fibra=0;
+    for (int i=0; i< dist.size(); i++){
+        if (dist[i] <= distanciaMax){
+            utp += dist[i] * precioUTP;
+        }else{
+            fibra += dist[i] * precioFibra;
+        }
+    }
+    return make_pair(utp,fibra);
+}
+
+void crearAristas(){
+    for (int i =0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            double dist = distancia(posiciones[i],posiciones[j]);
+            E.push_back({dist,i,j});
+        }
+    }
+}
 
 int main() {
     //leer
-    int c, oficina, distanciaMax, modems, precioUTP, precioFibra, x, y;
-    vector<pair<int,int>> posiciones;
-    vector<pair<double,double>> res;
+    int c, x, y,j=1;
+    cin >> c;
     while (c--){
-        cin >> oficina;
+        //input
+        cin >> n;
         cin >> distanciaMax;
         cin >> modems;
         cin >> precioUTP;
         cin >> precioFibra;
+        posiciones.assign(0,{});
+        E.assign(0,{});
         for (int i =0; i<n; i++){
             cin >> x; cin >> y;
             posiciones.push_back(make_pair(x,y));
         }
-        auto start = high_resolution_clock::now();
 
-        res.push_back(algoritmo());
+        //algoritmo
+        crearAristas();
+        pair<double,double> res = precios(kruskal());
 
         // salida
-        auto stop = high_resolution_clock::now();
-        float tiempo = duration_cast<milliseconds>(stop - start).count(); //milisegundos
-        tiempo = tiempo / 1000; // pasar a segundos
-        cout << "tardo: " << tiempo << " seg\n";
-    }
-
-    for (int i=0; i<res.size();i++){
-        cout << "Caso #" << i << ": " << setprecision(4) << res[i].first << " "  << setprecision(4) << res[i].second << endl;
+        printf("Caso #%d: %.3f %.3f\n",j,res.first,res.second);
+        j++;
     }
 
     return 0;
